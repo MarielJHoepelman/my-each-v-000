4ce9e4299e3698007f281e9f34f0b8e6650dad82
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-04-18 21:47:29 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,12 @@
 def my_each(array)
   if block_given? do
-  i = 0
-  while i < array.size
-    yield(array[i])
-    i = i + 1
-  end
-   array
+    i = 0
+    while i < array.size
+      yield(array[i])
+      i = i + 1
+    end
+    array
   else
-   "size is greater than 5!"
+    "size is greater than 5!"
   end
 end
